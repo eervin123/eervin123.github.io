f6b18438fac4b21cc21a0a3fdc5a7683d1a9b6fe
Eric Ervin <eervin@realityshares.com>
2022-01-15 09:02:21 -0800

    file structure

--- files.py
+++ files.py
@@ -1,4 +1,50 @@
-<!DOCTYPE html>
+"""
+Copyright 2020 glowinthedark
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Generate index.html files for
+all subdirectories in a directory tree.
+
+-handle symlinked files and folders: displayed with custom icons
+
+By default only the current folder is processed.
+
+Use -r or --recursive to process nested folders.
+"""
+
+import argparse
+import datetime
+import os
+import sys
+from pathlib import Path
+from urllib.parse import quote
+
+DEFAULT_OUTPUT_FILE = 'file_tree_test.html'
+
+
+def process_dir(top_dir, opts):
+    glob_patt = opts.filter or '*'
+
+    path_top_dir: Path
+    path_top_dir = Path(top_dir)
+    index_file = None
+
+    index_path = Path(path_top_dir, opts.output_file)
+
+    if opts.verbose:
+        print(f'Traversing dir {path_top_dir.absolute()}')
+
+    try:
+        index_file = open(index_path, 'w')
+    except Exception as e:
+        print('cannot create file %s %s' % (index_path, e))
+        return
+
+    index_file.write("""<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -259,7 +305,9 @@
     </defs>
     </svg>
 <header>
-    <h1>eervin123.github.io</h1>
+    <h1>"""
+                     f'{path_top_dir.name}'
+                     """</h1>
                  </header>
                  <main>
                  <div class="listing">
@@ -284,127 +332,154 @@
                              <td class="hideable">&mdash;</td>
                              <td class="hideable"></td>
                          </tr>
-                 
+                 """)
+
+    # sort dirs first
+    sorted_entries = sorted(path_top_dir.glob(glob_patt), key=lambda p: (p.is_file(), p.name))
+
+    entry: Path
+    for entry in sorted_entries:
+
+        # don't include index.html in the file listing
+        if entry.name.lower() == opts.output_file.lower():
+            continue
+
+        if entry.is_dir() and opts.recursive:
+            process_dir(entry, opts)
+
+        # From Python 3.6, os.access() accepts path-like objects
+        if (not entry.is_symlink()) and not os.access(str(entry), os.W_OK):
+            print(f"*** WARNING *** entry {entry.absolute()} is not writable! SKIPPING!")
+            continue
+        if opts.verbose:
+            print(f'{entry.absolute()}')
+
+        size_bytes = -1  ## is a folder
+        size_pretty = '&mdash;'
+        last_modified = '-'
+        last_modified_human_readable = '-'
+        last_modified_iso = ''
+        try:
+            if entry.is_file():
+                size_bytes = entry.stat().st_size
+                size_pretty = pretty_size(size_bytes)
+
+            if entry.is_dir() or entry.is_file():
+                last_modified = datetime.datetime.fromtimestamp(entry.stat().st_mtime).replace(microsecond=0)
+                last_modified_iso = last_modified.isoformat()
+                last_modified_human_readable = last_modified.strftime("%c")
+
+        except Exception as e:
+            print('ERROR accessing file name:', e, entry)
+            continue
+
+        entry_path = str(entry.name)
+
+        if entry.is_dir() and not entry.is_symlink():
+            entry_type = 'folder'
+            if os.name not in ('nt',):
+                # append trailing slash to dirs, unless it's windows
+                entry_path = os.path.join(entry.name, '')
+
+        elif entry.is_dir() and entry.is_symlink():
+            entry_type = 'folder-shortcut'
+            print('dir-symlink', entry.absolute())
+
+        elif entry.is_file() and entry.is_symlink():
+            entry_type = 'file-shortcut'
+            print('file-symlink', entry.absolute())
+
+        else:
+            entry_type = 'file'
+
+        index_file.write(f"""
         <tr class="file">
             <td></td>
             <td>
-                <a href=".git/">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#folder"></use></svg>
-                    <span class="name">.git</span>
+                <a href="{quote(entry_path)}">
+                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#{entry_type}"></use></svg>
+                    <span class="name">{entry.name}</span>
                 </a>
             </td>
-            <td data-order="-1">&mdash;</td>
-            <td class="hideable"><time datetime="2022-01-15T08:12:02">Sat Jan 15 08:12:02 2022</time></td>
+            <td data-order="{size_bytes}">{size_pretty}</td>
+            <td class="hideable"><time datetime="{last_modified_iso}">{last_modified_human_readable}</time></td>
             <td class="hideable"></td>
         </tr>
+""")
 
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="_posts/">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#folder"></use></svg>
-                    <span class="name">_posts</span>
-                </a>
-            </td>
-            <td data-order="-1">&mdash;</td>
-            <td class="hideable"><time datetime="2022-01-15T07:51:33">Sat Jan 15 07:51:33 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="backtests/">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#folder"></use></svg>
-                    <span class="name">backtests</span>
-                </a>
-            </td>
-            <td data-order="-1">&mdash;</td>
-            <td class="hideable"><time datetime="2022-01-15T08:31:36">Sat Jan 15 08:31:36 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href=".DS_Store">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">.DS_Store</span>
-                </a>
-            </td>
-            <td data-order="6148">6 KB</td>
-            <td class="hideable"><time datetime="2022-01-15T08:07:49">Sat Jan 15 08:07:49 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href=".gitignore">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">.gitignore</span>
-                </a>
-            </td>
-            <td data-order="1799">1 KB</td>
-            <td class="hideable"><time datetime="2022-01-15T06:12:52">Sat Jan 15 06:12:52 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="404.md">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">404.md</span>
-                </a>
-            </td>
-            <td data-order="122">122 bytes</td>
-            <td class="hideable"><time datetime="2022-01-15T08:01:10">Sat Jan 15 08:01:10 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="About.md">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">About.md</span>
-                </a>
-            </td>
-            <td data-order="1779">1 KB</td>
-            <td class="hideable"><time datetime="2022-01-15T08:05:47">Sat Jan 15 08:05:47 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="README.md">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">README.md</span>
-                </a>
-            </td>
-            <td data-order="1721">1 KB</td>
-            <td class="hideable"><time datetime="2022-01-15T08:15:04">Sat Jan 15 08:15:04 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
-        <tr class="file">
-            <td></td>
-            <td>
-                <a href="_config.yml">
-                    <svg width="1.5em" height="1em" version="1.1" viewBox="0 0 265 323"><use xlink:href="#file"></use></svg>
-                    <span class="name">_config.yml</span>
-                </a>
-            </td>
-            <td data-order="132">132 bytes</td>
-            <td class="hideable"><time datetime="2022-01-15T06:19:38">Sat Jan 15 06:19:38 2022</time></td>
-            <td class="hideable"></td>
-        </tr>
-
+    index_file.write("""
             </tbody>
         </table>
     </div>
 </main>
 </body>
-</html>
+</html>""")
+    if index_file:
+        index_file.close()
+
+
+# bytes pretty-printing
+UNITS_MAPPING = [
+    (1024 ** 5, ' PB'),
+    (1024 ** 4, ' TB'),
+    (1024 ** 3, ' GB'),
+    (1024 ** 2, ' MB'),
+    (1024 ** 1, ' KB'),
+    (1024 ** 0, (' byte', ' bytes')),
+]
+
+
+def pretty_size(bytes, units=UNITS_MAPPING):
+    """Human-readable file sizes.
+
+    ripped from https://pypi.python.org/pypi/hurry.filesize/
+    """
+    for factor, suffix in units:
+        if bytes >= factor:
+            break
+    amount = int(bytes / factor)
+
+    if isinstance(suffix, tuple):
+        singular, multiple = suffix
+        if amount == 1:
+            suffix = singular
+        else:
+            suffix = multiple
+    return str(amount) + suffix
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='''DESCRIPTION:
+    Generate directory index files (recursive is OFF by default).
+    Start from current dir or from folder passed as first positional argument.
+    Optionally filter by file types with --filter "*.py". ''')
+
+    parser.add_argument('top_dir',
+                        nargs='?',
+                        action='store',
+                        help='top folder from which to start generating indexes, '
+                             'use current folder if not specified',
+                        default=os.getcwd())
+
+    parser.add_argument('--filter', '-f',
+                        help='only include files matching glob',
+                        required=False)
+
+    parser.add_argument('--output-file', '-o',
+                        metavar='filename',
+                        default=DEFAULT_OUTPUT_FILE,
+                        help=f'Custom output file, by default "{DEFAULT_OUTPUT_FILE}"')
+
+    parser.add_argument('--recursive', '-r',
+                        action='store_true',
+                        help="recursively process nested dirs (FALSE by default)",
+                        required=False)
+
+    parser.add_argument('--verbose', '-v',
+                        action='store_true',
+                        help='***WARNING: can take longer time with complex file tree structures on slow terminals***'
+                             ' verbosely list every processed file',
+                        required=False)
+
+    config = parser.parse_args(sys.argv[1:])
+    process_dir(config.top_dir, config)
